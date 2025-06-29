3d123a63909181b78346e6360d94f30573862926
akaMiWP <bangkokappdevelopment@gmail.com>
2025-06-29 17:45:18 +0700

    Modifed the page and added the lib for icons

--- my-expo-app/components/coffee/CoffeeOrderPage.tsx
+++ my-expo-app/components/coffee/CoffeeOrderPage.tsx
@@ -5,6 +5,7 @@ const CoffeeOrderPage = () => {
   return (
     <SafeAreaView>
       <Header />
+      <ContentList />
     </SafeAreaView>
   );
 };
@@ -15,6 +16,36 @@ const Header = () => {
     <View className="flex gap-y-4 px-3">
       <Text className="bg-green-200 text-3xl font-bold">Order</Text>;
       <Text>Exclusive for Starbucks® Rewards members.</Text>
+    </View>
+  );
+};
+
+// Content
+const ContentList = () => {
+  return (
+    <View className="mt-8 flex gap-y-4">
+      <ContentItem title="Delivery" subtitle="Order your favorites that delivers near you." />;
+      <ContentItem
+        title="In-store Pickup"
+        subtitle="Save time by ordering ahead and paying on the app."
+      />
+      ;
+      <ContentItem
+        title="Order to Table"
+        subtitle="Scan the QR code to order at your table and wait to be served."
+      />
+      ;
+    </View>
+  );
+};
+
+const ContentItem = ({ title, subtitle }: { title: string; subtitle: string }) => {
+  return (
+    <View className="px-3 py-2">
+      <View className="flex gap-y-2 rounded-md border-2 border-gray-200 px-6 py-6">
+        <Text className="text-lg font-semibold">{title}</Text>
+        <Text className="text-sm">{subtitle}</Text>
+      </View>
     </View>
   );
 };
